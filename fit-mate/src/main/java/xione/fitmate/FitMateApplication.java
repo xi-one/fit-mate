5365004c622f185e3b736c2fae201cc860ac9c7e
xi-one <ssly1133@naver.com>
2023-09-05 01:19:26 +0900

    default setting

---
+++ fit-mate/src/main/java/xione/fitmate/FitMateApplication.java
@@ -0,0 +1,13 @@
+package xione.fitmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitMateApplication.class, args);
+	}
+
+}
---
+++ fit-mate/src/test/java/xione/fitmate/FitMateApplicationTests.java
@@ -0,0 +1,13 @@
+package xione.fitmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
